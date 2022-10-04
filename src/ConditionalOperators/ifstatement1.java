119b7a5cab9fca751534a4f7976a474478fd84aa
Bakary Sylla <syllabakary2002@gmail.com>
2022-10-03 18:21:00 -0700

    First statement

---
+++ src/ConditionalOperators/ifstatement1.java
@@ -0,0 +1,15 @@
+package ConditionalOperators;
+
+public class ifstatement1 {
+
+    public static void main(String[] args) {
+        int x = 5;
+        if (x > 0){
+            System.out.println("X is a great than 0");
+        }
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("the value of i is: " + i);
+        }
+    }
+}
