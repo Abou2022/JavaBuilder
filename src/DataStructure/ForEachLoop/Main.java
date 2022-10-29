fa9ec6df174d12acd15ba974ad684c61bda6763a
Bakary Sylla <syllabakary2002@gmail.com>
2022-10-28 17:38:50 -0700

    Main and ListCreator update

--- src/DataStructure/ForEachLoop/Main.java
+++ src/DataStructure/ForEachLoop/Main.java
@@ -1,7 +1,22 @@
 package DataStructure.ForEachLoop;
 
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        List<Integer> intListExample = ListCreator.generateIntArrayList();
+        System.out.println("Integers you added to the first list:");
+        for (Integer num : intListExample) {
+            System.out.println(num);
+        }
+
+        List<Integer> intList = ListCreator.generateIntArrayList();
+        int max = intList.get(0);
+
+        for(Integer maximum : intList){
+            System.out.println("Maximum number in second list: " + maximum);
+        }
+
+        System.out.println("Maximum number in second list: " + max);
 
     }
 }
