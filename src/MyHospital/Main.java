2aefd80400ae9fa046aae4fe96d87eaf327cae1f
Bakary Sylla <syllabakary2002@gmail.com>
2022-10-30 07:00:51 -0700

    ReadMe update

--- src/MyHospital/Main.java
+++ src/MyHospital/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
        ArrayList <Bill> newBill = new ArrayList<>();
         Patient myNewBill = new Patient("John", newBill);
-
+        System.out.println(myNewBill);
     }
 }
