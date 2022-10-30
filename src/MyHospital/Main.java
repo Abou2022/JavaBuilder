91c9859f8f8935a0ffc578845bfd360768f8a5fe
Bakary Sylla <syllabakary2002@gmail.com>
2022-10-30 06:32:35 -0700

    Added Readme.md file

--- src/MyHospital/Main.java
+++ src/MyHospital/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
        ArrayList <Bill> newBill = new ArrayList<>();
-
+        Patient myNewBill = new Patient("John", newBill);
 
     }
 }
