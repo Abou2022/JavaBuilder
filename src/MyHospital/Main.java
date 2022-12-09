63e16b6dffcf42eed8272782faac0abca30e8b06
Bakary Sylla <syllabakary2002@gmail.com>
2022-12-08 19:44:50 -0800

    Readme update

--- src/MyHospital/Main.java
+++ src/MyHospital/Main.java
@@ -16,6 +16,7 @@ class Main {
         //Create a new Bill object for John with amount 99.95 and     date June 15 2021
         Bill myfacture = new Bill(99.95, "June 15 2021");
         System.out.println(myfacture);
+
         //Need to set amount
         myfacture.setAmount(99.95);
         //get amount
